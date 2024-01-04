12ec198e5de0a270fd89b4b018c94a1a1b2da7af
Walber Vaz da Silva <wvs.walber@gmail.com>
2024-01-03 21:29:25 -0300

    feat: add class-transformer and class-validator dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import * as compression from 'compression';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
   app.use(compression());
 
