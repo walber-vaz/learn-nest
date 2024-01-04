829c771662b3ae14ba14abc19910bfa4d27d1af9
Walber Vaz da Silva <wvs.walber@gmail.com>
2024-01-04 17:06:21 -0300

    feat: add .env file, update VSCode settings, and add Prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PrismaModule],
   controllers: [],
   providers: [],
   exports: [],
